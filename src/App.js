d45a28d455ec7eb2d60cd16dfeeda8e5c04bd7cc
Awakuruf <dfurukawa@kokoryugaku.com>
2023-03-03 18:50:33 -0800

    added new classes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import { Button } from "@mui/material";
+import * as React from "react";
+import { Button, IconButton } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import "./App.css"
+import { palette } from '@mui/system';
 
 
 
 function App() {
+  // const pages = ["Work", "About"];
+
   return (
     <div>
-      Hello
-      <Button variant="contained"> Contained </Button>
+    <Button className="github" variant="contained" startIcon={<GitHubIcon/>}></Button>
+    <Button className="resume_button" variant="contained" endIcon={<DownloadIcon/>}>Resume</Button>
+    <IconButton aria-label="github" color = "primary" sx={{
+      borderRadius: 1,
+      bgcolor: "primary",
+    }}>
+      <GitHubIcon/>
+    </IconButton>
+    <IconButton aria-label="github" color = "primary" sx={{
+      borderRadius: 1,
+      bgcolor: "primary",
+    }}>
+      <LinkedInIcon/>
+    </IconButton>
+
     </div>
+
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary:{
+            main: "#2A28FF",
+        },
+        secondary: {
+            main: "1A75CA",
+        },
+        otherColor: {
+            main: "#fff"
+        }
+    }
+})
